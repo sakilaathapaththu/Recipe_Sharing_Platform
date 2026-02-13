aa65752a7f7a69d6e39e1d89b89ddbc251c0a4c6
Athapaththu <sakila.atapattu@gmail.com>
2026-02-13 13:02:19 +0530

    add main.js to api caling

--- backend/main.py
+++ backend/main.py
@@ -16,11 +16,13 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://recipe-sharing-platform-zeta-kohl.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # serve uploaded images
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
